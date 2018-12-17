0c5579671ebbf5070cc49f1a09b798caa4b00c67
VeraTarasova <webtarasoff@gmail.com>
2018-12-17 11:56:32 +0200

    final commit

--- ClientApp/dist/index.php
+++ ClientApp/dist/index.php
@@ -1,14 +1,1 @@
-<!DOCTYPE html><html lang=en><head><meta charset=utf-8><meta http-equiv=X-UA-Compatible content="IE=edge">
-<meta name=viewport content="width=device-width,initial-scale=1">
-<link rel=icon href=/favicon.ico>
-<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel=stylesheet><title>Wilo</title>
-<link href=/wiloCore/css/app.ee0f3e2d.css rel=preload as=style>
-<link href=/wiloCore/css/chunk-vendors.9c8e3772.css rel=preload as=style>
-
-<link href=/wiloCore/css/chunk-vendors.9c8e3772.css rel=stylesheet>
-<link href=/wiloCore/css/app.ee0f3e2d.css rel=stylesheet></head><body>
-<noscript>
-    <strong>We're sorry but hello-world doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
-</noscript><div id=app></div>
-<script src=/wiloCore/js/chunk-vendors.1cd6accb.js></script><script src=/wiloCore/js/app.20c423bd.js></script>
-</body></html>
+<!DOCTYPE html><html lang=en><head><meta charset=utf-8><meta http-equiv=X-UA-Compatible content="IE=edge"><meta name=viewport content="width=device-width,initial-scale=1"><link rel=icon href=/favicon.ico><link href="https://fonts.googleapis.com/css?family=Open+Sans" rel=stylesheet><title>Wilo</title><link href=/css/app.8a15d807.css rel=preload as=style><link href=/css/chunk-vendors.9c8e3772.css rel=preload as=style><link href=/js/app.7b5374da.js rel=preload as=script><link href=/js/chunk-vendors.66686550.js rel=preload as=script><link href=/css/chunk-vendors.9c8e3772.css rel=stylesheet><link href=/css/app.8a15d807.css rel=stylesheet></head><body><noscript><strong>We're sorry but hello-world doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id=app></div><script src=/js/chunk-vendors.66686550.js></script><script src=/js/app.7b5374da.js></script></body></html>
